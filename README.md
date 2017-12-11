8fbbbf02ab19b83e9446dc85287c87d3db4de2c9
Dong Liu <edongliu@gmail.com>
2017-12-10 19:58:54 -0800

    add main py, menu and command

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # sublime-readonly
-default readonly mode and toggle for sublime text
+Set default readonly mode by `"read_only_mode" = true` in your setting. Toggle a view's readonly mode in context menu or via command.
---
+++ readonly.py
@@ -0,0 +1,22 @@
+import sublime
+import sublime_plugin
+
+
+class ToggleReadonlyModeCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    if (self.view.is_read_only()):
+      self.view.set_read_only(False)
+      self.view.set_status('read_only_mode', 'writeable')
+      sublime.status_message("View " + str(self.view.file_name()) + " is writeable.")
+    else:
+      self.view.set_read_only(True)
+      self.view.set_status('read_only_mode', 'readonly')
+      sublime.status_message("View " + str(self.view.file_name()) + " is readonly.")
+
+
+class ToggleReadonlyListener(sublime_plugin.EventListener):
+   def on_load(self, view):
+      if view.settings().get('read_only_mode'):
+        view.set_read_only(True)
+        view.set_status('read_only_mode', 'readonly')
+        sublime.status_message("View " + str(view.file_name()) + " is readonly.")
