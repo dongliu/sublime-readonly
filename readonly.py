0da022c4e8f20a5bd33e66d08f52ec3a5a9c9e21
Dong Liu <edongliu@gmail.com>
2018-02-11 09:00:47 -0800

    use view.window().status_message, sublime.status_message did the same

--- readonly.py
+++ readonly.py
@@ -7,11 +7,11 @@ class ToggleReadonlyModeCommand(sublime_plugin.TextCommand):
     if (self.view.is_read_only()):
       self.view.set_read_only(False)
       self.view.set_status('read_only_mode', 'writeable')
-      sublime.status_message("View " + str(self.view.file_name()) + " is writeable.")
+      self.view.window().status_message("View " + str(self.view.file_name()) + " is writeable.")
     else:
       self.view.set_read_only(True)
       self.view.set_status('read_only_mode', 'readonly')
-      sublime.status_message("View " + str(self.view.file_name()) + " is readonly.")
+      self.view.window().status_message("View " + str(self.view.file_name()) + " is readonly.")
 
 
 class ToggleReadonlyListener(sublime_plugin.EventListener):
@@ -19,4 +19,4 @@ class ToggleReadonlyListener(sublime_plugin.EventListener):
       if view.settings().get('read_only_mode'):
         view.set_read_only(True)
         view.set_status('read_only_mode', 'readonly')
-        sublime.status_message("View " + str(view.file_name()) + " is readonly.")
+        view.window().status_message("View " + str(view.file_name()) + " is readonly.")
